790463dda3395fb465c5e1fd040e4f8c0b808db4
Ming <mm@bigecko.com>
2017-12-06 12:57:56 +0800

    build

--- build.bat
+++ build.bat
@@ -12,7 +12,7 @@ del KeePassHttp.plgx
 del mono\KeePassHttp.dll
 rd /s /q KeePassHttp\bin
 rd /s /q KeePassHttp\obj
-"%ProgramFiles(x86)%\KeePass Password Safe 2\KeePass.exe" --plgx-create %base%KeePassHttp
+D:\Soft\KeePass\KeePass.exe --plgx-create %base%KeePassHttp
 %netframework%MSBuild.exe /target:clean KeePassHttp.sln
 %netframework%MSBuild.exe /p:Configuration=Release KeePassHttp.sln
 copy /y KeePassHttp\bin\Release\KeePassHttp.dll mono
